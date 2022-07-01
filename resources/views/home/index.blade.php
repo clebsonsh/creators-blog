c8349fe615e6331baa905e11a60b187b21289e16
Clebson Moura <clebson.sh@gmail.com>
2022-07-01 13:46:31 -0300

    register link

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,8 @@
     @auth
     <a href="{{ route('post.index') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
     @else
-    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 px-4 underline">Log in</a>
+    <a href="{{ route('register') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
     @endauth
   </div>
   @endif
